68487bc39536871db112227c1939b063ff465a74
ohdair <ohdiar@gmail.com>
2022-03-02 23:23:55 +0900

    틱택톡 추가

---
+++ LottoGenerator/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    entry: {
+        app: path.join(__dirname, 'main.js'),
+    },
+    module: { 
+        rules: [{
+            test: /\.vue$/,
+            use: 'vue-loader',
+        }, {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        esModule: false
+                    }
+                }
+            ]
+        }],
+
+    },
+    plugins : [
+        new VueLoaderPlugin(),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist/'
+    },
+    devServer: {
+        devMiddleware: {publicPath: '/dist'},
+        static: {directory: path.resolve(__dirname)},
+        hot: true
+    }
+}
---
+++ TicTacToe/TrComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <tr>
+        <td-component v-for="(cellData, index) in rowData" :key="index" :cell-data="cellData" :cell-index="index" :row-index="rowIndex"></td-component>
+    </tr>
+</template>
+<script>
+    import TdComponent from './TdComponent';
+
+    export default {
+        components: {
+            TdComponent,
+        },
+        props: {
+            rowData: Array,
+            rowIndex: Number,
+        },
+    }
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
